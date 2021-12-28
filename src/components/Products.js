dea25a019f5564c7c0b49ddad4b895a4dfa8c341
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-12-28 16:00:04 +0530

    created state variables in Products.js component

--- src/components/Products.js
+++ src/components/Products.js
@@ -38,73 +38,18 @@ const Products = () => {
     _id: 'PmInA797xJhMIPti',
   };
 
-  // TODO: CRIO_TASK_MODULE_PRODUCTS - Fetch products data and store it
-  /**
-   * Make API call to get the products list and store it to display the products
-   *
-   * @returns { Array.<Product> }
-   *      Array of objects with complete data on all available products
-   *
-   * API endpoint - "GET /products"
-   *
-   * Example for successful response from backend:
-   * HTTP 200
-   * [
-   *      {
-   *          "name": "iPhone XR",
-   *          "category": "Phones",
-   *          "cost": 100,
-   *          "rating": 4,
-   *          "image": "https://i.imgur.com/lulqWzW.jpg",
-   *          "_id": "v4sLtEcMpzabRyfx"
-   *      },
-   *      {
-   *          "name": "Basketball",
-   *          "category": "Sports",
-   *          "cost": 100,
-   *          "rating": 5,
-   *          "image": "https://i.imgur.com/lulqWzW.jpg",
-   *          "_id": "upLK9JbQ4rMhTwt4"
-   *      }
-   * ]
-   *
-   * Example for failed response from backend:
-   * HTTP 500
-   * {
-   *      "success": false,
-   *      "message": "Something went wrong. Check the backend console for more details"
-   * }
-   */
+  const cartRef = React.createRef();
+
+  const debounceTimeout = 0;
+  const products = [];
+  const [loading, setLoading] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   const performAPICall = async () => {};
 
-  // TODO: CRIO_TASK_MODULE_PRODUCTS - Implement search logic
-  /**
-   * Definition for search handler
-   * This is the function that is called on adding new search keys
-   *
-   * @param {string} text
-   *    Text user types in the search bar. To filter the displayed products based on this text.
-   *
-   * @returns { Array.<Product> }
-   *      Array of objects with complete data on filtered set of products
-   *
-   * API endpoint - "GET /products/search?value=<search-query>"
-   *
-   */
   const performSearch = async (text) => {};
 
-  // TODO: CRIO_TASK_MODULE_PRODUCTS - Optimise API calls with debounce search implementation
-  /**
-   * Definition for debounce handler
-   * With debounce, this is the function to be called whenever the user types text in the searchbar field
-   *
-   * @param {{ target: { value: string } }} event
-   *    JS event object emitted from the search input field
-   *
-   * @param {NodeJS.Timeout} debounceTimeout
-   *    Timer id set for the previous debounce call
-   *
-   */
   const debounceSearch = (event, debounceTimeout) => {};
 
   return (
