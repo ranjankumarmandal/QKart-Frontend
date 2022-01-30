0776bee1392ffb17a0c3994b3520ba1163b6ed64
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-30 07:43:36 +0530

    added fetchCart functionality in Product.js component

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,18 +14,7 @@ import Footer from './Footer';
 import Header from './Header';
 import './Products.css';
 import ProductCard from './ProductCard';
-
-// Definition of Data Structures used
-/**
- * @typedef {Object} Product - Data on product available to buy
- *
- * @property {string} name - The name or title of the product
- * @property {string} category - The category that the product belongs to
- * @property {number} cost - The price to buy the product
- * @property {number} rating - The aggregate rating of the product (integer out of five)
- * @property {string} image - Contains URL for the product image
- * @property {string} _id - Unique ID for the product
- */
+import Cart from './Cart';
 
 const Products = () => {
   const product = {
@@ -39,7 +28,7 @@ const Products = () => {
   };
 
   const cartRef = React.createRef();
-  const debounceTimeout = 0;
+  let debounceTimeout = 0;
   let products = [];
   const [loading, setLoading] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
@@ -47,8 +36,8 @@ const Products = () => {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const { enqueueSnackbar } = useSnackbar();
 
-  const debounceSearch = (event) => {
-    const value = event.target.value;
+  const debounceSearch = (value) => {
+    // const value = event.target.value;
 
     if (debounceTimeout) {
       clearTimeout(debounceTimeout);
@@ -56,7 +45,7 @@ const Products = () => {
 
     debounceTimeout = setTimeout(() => {
       performSearch(value);
-    }, 300);
+    }, 500);
   };
 
   const performSearch = async (text) => {
@@ -102,6 +91,35 @@ const Products = () => {
     }
   };
 
+  const fetchCart = async () => {
+    try {
+      const config = {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      };
+      let response = await axios.get(`${config.endpoint}/cart`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      });
+      console.log(response);
+      // products = response.data;
+      // console.log(products.slice());
+      // setFilteredProducts(products.slice());
+    } catch (error) {
+      console.log(error.response);
+      if (error.response) {
+        console.log(error.response.data.message);
+        enqueueSnackbar(
+          error.response.data.message || 'No Products in cart Found',
+          { variant: 'error' }
+        );
+      } else {
+        enqueueSnackbar(
+          'Could not update cart. Check that the backend is running, reachable and returns valid JSON.',
+          { variant: 'error' }
+        );
+      }
+    }
+  };
+
   useEffect(() => {
     getProducts();
 
@@ -112,7 +130,7 @@ const Products = () => {
 
   return (
     <div>
-      <Header search={performSearch}></Header>
+      <Header search={debounceSearch}></Header>
 
       {/* Search view for mobiles */}
       <TextField
@@ -127,44 +145,95 @@ const Products = () => {
           ),
         }}
         placeholder='Search for items/categories'
+        onChange={(e) => debounceSearch(e.target.value)}
         name='search'
       />
-      <Grid container>
-        <Grid item className='product-grid'>
-          <Box className='hero'>
-            <p className='hero-heading'>
-              India’s <span className='hero-highlight'>FASTEST DELIVERY</span>{' '}
-              to your door step
-            </p>
-          </Box>
-        </Grid>
-      </Grid>
+      {localStorage.getItem('username') ? (
+        <div>
+          <Grid container>
+            <Grid item xs={12} sm={9}>
+              <Grid container>
+                <Grid item className='product-grid'>
+                  <Box className='hero'>
+                    <p className='hero-heading'>
+                      India’s{' '}
+                      <span className='hero-highlight'>FASTEST DELIVERY</span>{' '}
+                      to your door step
+                    </p>
+                  </Box>
+                </Grid>
+              </Grid>
 
-      {loading ? (
-        <div style={{ textAlign: 'center', padding: '150px' }}>
-          <CircularProgress />
-          <p>Loading Products...</p>
-        </div>
-      ) : productFound ? (
-        <Grid
-          container
-          spacing={{ xs: 2, md: 4 }}
-          columns={{ xs: 4, sm: 8, md: 16 }}
-          style={{ marginTop: '0.5px', marginBottom: '20px' }}
-        >
-          {filteredProducts.map((product) => (
-            <Grid item xs={2} sm={4} md={4} key={product._id}>
-              <ProductCard product={product} />
+              {loading ? (
+                <div style={{ textAlign: 'center', padding: '150px' }}>
+                  <CircularProgress />
+                  <p>Loading Products...</p>
+                </div>
+              ) : productFound ? (
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 4 }}
+                  columns={{ xs: 4, sm: 8, md: 16 }}
+                  style={{ marginTop: '0.5px', marginBottom: '20px' }}
+                >
+                  {filteredProducts.map((product) => (
+                    <Grid item xs={2} sm={4} md={4} key={product._id}>
+                      <ProductCard product={product} />
+                    </Grid>
+                  ))}
+                </Grid>
+              ) : (
+                <div style={{ textAlign: 'center', padding: '150px' }}>
+                  <SentimentDissatisfied />
+                  <p>No products found</p>
+                </div>
+              )}
             </Grid>
-          ))}
-        </Grid>
-      ) : (
-        <div style={{ textAlign: 'center', padding: '150px' }}>
-          <SentimentDissatisfied />
-          <p>No products found</p>
+            <Grid item xs={12} sm={3} className='cartClass'>
+              <Cart />
+            </Grid>
+          </Grid>
         </div>
-      )}
+      ) : (
+        <>
+          <Grid container>
+            <Grid item className='product-grid'>
+              <Box className='hero'>
+                <p className='hero-heading'>
+                  India’s{' '}
+                  <span className='hero-highlight'>FASTEST DELIVERY</span> to
+                  your door step
+                </p>
+              </Box>
+            </Grid>
+          </Grid>
 
+          {loading ? (
+            <div style={{ textAlign: 'center', padding: '150px' }}>
+              <CircularProgress />
+              <p>Loading Products...</p>
+            </div>
+          ) : productFound ? (
+            <Grid
+              container
+              spacing={{ xs: 2, md: 4 }}
+              columns={{ xs: 4, sm: 8, md: 16 }}
+              style={{ marginTop: '0.5px', marginBottom: '20px' }}
+            >
+              {filteredProducts.map((product) => (
+                <Grid item xs={2} sm={4} md={4} key={product._id}>
+                  <ProductCard product={product} />
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <div style={{ textAlign: 'center', padding: '150px' }}>
+              <SentimentDissatisfied />
+              <p>No products found</p>
+            </div>
+          )}
+        </>
+      )}
       <Footer />
     </div>
   );
